852e34b090dd68e18826ca8d4a90257438d6bcff
Aron <aron.liiv@hotmail.com>
2022-12-07 17:12:34 +0200

    frontend setup

---
+++ our_frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>Our homepage</h3>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
