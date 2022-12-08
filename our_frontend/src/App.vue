cd2ea08b9fde6f6e2c3a90b57b6897c664ca6e87
jkirikal <johan@vhk.ee>
2022-12-08 17:42:26 +0200

    completed contact us page and started with main

--- our_frontend/src/App.vue
+++ our_frontend/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+<!--     <router-link to="/signup">SignUp</router-link> | -->
+<!--     <router-link to="/login">LogIn</router-link> | -->
+    <router-link to="/contact">Contact us</router-link>
+  </nav>
+  <router-view />
 </template>
 
 <style>
---
+++ our_frontend/src/views/ContactView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contact">
+        <h1>This is a contact us page</h1>
+        <div>
+            <p>
+                We are 3 students completing the web application development course and this is our homework 4.
+            </p>
+            <p>
+                You can contact us on email: <span>katjajohanaron@mail.mail</span>
+            </p>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ContactView",
+  }
+  </script>
+  
