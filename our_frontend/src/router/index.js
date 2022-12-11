645cb953062c6819388a06a2b083bed49dc79cf9
jkirikal <johan@vhk.ee>
2022-12-11 17:38:20 +0200

    main page, create post, router and post pages created

--- our_frontend/src/router/index.js
+++ our_frontend/src/router/index.js
@@ -2,25 +2,37 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import auth from "../auth";
 import ContactView from "../views/ContactView.vue";
+import Post from "../views/Post.vue";
+import AddPost from "../views/AddPost.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-    beforeEnter: async(to, from, next) => {
-        let authResult = await auth.authenticated();
-        if (!authResult) {
-            next('/login')
-        } else {
-            next();
-        }
-    }
+    // beforeEnter: async(to, from, next) => {
+    //     let authResult = await auth.authenticated();
+    //     if (!authResult) {
+    //         next('/login')
+    //     } else {
+    //         next();
+    //     }
+    // }
 },
 {
   path: "/contact",
   name: "Contact Us",
   component: ContactView,
+},
+{
+  path: "/post/:id",
+  name: "Post",
+  component: Post,
+},
+{
+  path: "/addpost",
+  name: "AddPost",
+  component: AddPost,
 },
 ]
 
